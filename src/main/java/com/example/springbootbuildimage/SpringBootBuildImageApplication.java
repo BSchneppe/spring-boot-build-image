44ad31d9f7fadd288ca7901cc82ce7475cfaec2a
Benedikt Schneppe <benedikt@flowers-software.de>
2024-04-09 14:01:08 +0200

    initial commit

---
+++ src/main/java/com/example/springbootbuildimage/SpringBootBuildImageApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootbuildimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBuildImageApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootBuildImageApplication.class, args);
+  }
+}
